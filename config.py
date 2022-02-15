6f977dd700fc9b4b82ba831d964b55b39079bf82
Suleiman <suleimanmukhtarov@gmail.com>
2022-02-15 23:52:44 +0500

    Add database connection

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5115569610:AAFMk_FoJeb26153yxrc2Nu081C7POuUGWo"
 APP_URL = "https://database-b.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://htxwllggoonzkp:7717cc54ea7c52ff922b2f8857a36ee75d9f99978ca10ea827bc16f4e0824210@ec2-52-30-133-191.eu-west-1.compute.amazonaws.com:5432/dufa0nv2klm8f"
